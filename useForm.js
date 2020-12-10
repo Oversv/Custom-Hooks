baa39fb0f037aa30deba4159e0b5da3848ade724
oversv <albertomdv@gmail.com>
2020-12-10 10:01:59 +0000

    custom hooks

---
+++ useForm.js
@@ -0,0 +1,24 @@
+import { useState } from "react"
+/**
+ * Custom Hook - useForm
+ * @param {object} initialState object with form fields {name: '', surname: '', ...}
+ * return an array
+ */
+export const useForm = (initialState={}) => {
+
+    const [values, setValues] = useState(initialState)
+
+    const reset = () =>{
+        setValues(initialState)
+    }
+
+    const handleInputChange = ({target}) =>{
+ 
+        setValues({
+            ...values,
+            [target.name]: target.value
+        })
+    }
+
+    return [values, handleInputChange, reset]
+}
